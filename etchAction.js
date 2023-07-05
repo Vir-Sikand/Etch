08bf77cbdca1062e7cbae77610cfdba9bbae6d00
Vir-Sikand <sikandvir@gmail.com>
2023-07-05 15:52:43 -0400

    added etch functionality and added eraser button (not functional)

--- etchAction.js
+++ etchAction.js
@@ -1,6 +1,33 @@
 gridContainer = document.querySelector(".grid")
 
 for (i = 0; i < 16; i++) {
-    var div = document.createElement('div')
-    gridContainer.appendChild(div)
+    for (j = 0; j < 16; j++) {
+        var div = document.createElement('div')
+        div.classList.add('item')
+        gridContainer.appendChild(div)
+    }
 }
+
+isMouseDown = false
+gridItems = document.querySelectorAll('.item')
+gridItems.forEach(function(gridItem) {
+    gridItem.addEventListener('mouseover', function(event) {
+        gridItem.classList.add('filled');
+    })
+    gridItem.addEventListener('mouseout', function(event) {
+        gridItem.classList.remove('filled');
+    })
+    gridItem.addEventListener('mousedown', function(event) {
+        isMouseDown = true
+        gridItem.classList.add('filledPerm');
+    })
+
+    gridItem.addEventListener('mousemove', function(event) {
+        if (isMouseDown == true) {
+            gridItem.classList.add('filledPerm');
+        }
+    })
+    gridItem.addEventListener('mouseup', function(event) {
+        isMouseDown = false
+    })
+})
