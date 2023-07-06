f029795b5b153572b37fd7648df659784239305a
Vir-Sikand <sikandvir@gmail.com>
2023-07-06 16:35:18 -0400

    added ability to change grid size

--- etchAction.js
+++ etchAction.js
@@ -1,11 +1,54 @@
 gridContainer = document.querySelector(".grid")
+var dimensions = 16
+updateGrid(16)
 
-for (i = 0; i < 16; i++) {
-    for (j = 0; j < 16; j++) {
-        var div = document.createElement('div')
-        div.classList.add('item')
-        gridContainer.appendChild(div)
+slider = document.getElementById('slider')
+sliderValue = document.getElementById('sliderValue')
+
+slider.addEventListener('input', function() {
+    var value = parseInt(slider.value)
+    sliderValue.textContent = value.toString()
+    updateGrid(value)
+})
+
+
+function updateGrid(sideLength) {
+    gridContainer.innerHTML = ''
+    for (i = 0; i < sideLength; i++) {
+        for (j = 0; j < sideLength; j++) {
+            var div = document.createElement('div')
+            div.classList.add('item')
+            gridContainer.appendChild(div)
+        }
     }
+    var r = Math.floor(960/sideLength)
+    console.log(r)
+    gridContainer.style.gridTemplateColumns = `repeat(${sideLength}, ${r}px)`;
+    gridContainer.style.gridTemplateRows = `repeat(${sideLength}, ${r}px)`;
+
+    isMouseDown = false
+    gridItems = document.querySelectorAll('.item')
+    gridItems.forEach(function(gridItem) {
+    gridItem.addEventListener('mouseover', function(event) {
+        gridItem.classList.add('filled');
+    })
+    gridItem.addEventListener('mouseout', function(event) {
+        gridItem.classList.remove('filled');
+    })
+    gridItem.addEventListener('mousedown', function(event) {
+        isMouseDown = true
+        gridItem.classList.add('filledPerm');
+    })
+
+    gridItem.addEventListener('mousemove', function(event) {
+        if (isMouseDown == true) {
+            gridItem.classList.add('filledPerm');
+        }
+    })
+    gridItem.addEventListener('mouseup', function(event) {
+        isMouseDown = false
+    })
+})
 }
 
 isMouseDown = false
@@ -31,3 +74,11 @@ gridItems.forEach(function(gridItem) {
         isMouseDown = false
     })
 })
+
+clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', function(event) {
+    gridItems.forEach(function(gridItem) {
+        gridItem.classList.remove('filledPerm')
+    })
+})
+
